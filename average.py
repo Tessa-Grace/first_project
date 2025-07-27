c9c459eb47cc6132545ad0277c8b8c4bb5313578
Tessa Gerasimova <tessagerasimova@gmail.com>
2025-07-27 17:10:32 +0200

    -

--- average.py
+++ average.py
@@ -1,17 +1,15 @@
-def calculate_sum_and_average(numbers):
-    total_sum = 0
-    for num in numbers:
-        total_sum += num
+class Phone:
 
-    average = total_sum / len(numbers)
-    return total_sum, average
+    line_type = 'проводной'
 
-numbers = [10, 20, 30, 40, 50]
-total, avg = calculate_sum_and_average(numbers)
-print(f'Сумма чисел: {total}')
-print(f'Среднее значение: {avg}')
+    def __init__(self):
+        self.dial_type = 'дисковый'
 
-numbers = [11, 22, 33, 42, 51]
-total, avg = calculate_sum_and_average(numbers)
-print(f'Сумма чисел: {total}')
-print(f'Среднее значение: {avg}')
+
+# Создать объект rotary_phone.
+rotary_phone = Phone()
+# Создать объект rotary_phone_2.
+rotary_phone_2 = Phone()
+
+print(rotary_phone.line_type)
+print(rotary_phone_2.dial_type)
